d1e3f9c6a73cd06185474abd1c69cb424136bf86
masteribro <mohammedibrahim075@gmail.com>
2025-04-26 18:16:02 +0100

    Initial Commit

---
+++ git_profileTests/git_profileTests.swift
@@ -0,0 +1,16 @@
+//
+//  git_profileTests.swift
+//  git_profileTests
+//
+//  Created by Ibrahim Mohammed on 26/04/2025.
+//
+
+import Testing
+
+struct git_profileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
